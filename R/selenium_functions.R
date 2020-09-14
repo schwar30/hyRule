d76258aedd61f614bd4e1ab0e3ee2f052d6792b1
Robert Schwartz <rschwartz@kennedyc.com>
2020-09-14 16:36:30 -0500

    Added Selenium Functions

---
+++ R/selenium_functions.R
@@ -0,0 +1,25 @@
+#' Gets List of Relevant Chrome Versions
+#'
+#' @description This is a helper function to be used alongside RSelenium. Returns a list of 
+#' available chrome versions on the user's computer
+#'
+#' @details RSelenium often crashes when there is an incorrect chrome association, so this
+#' is a great helper function that allows us to more quickly and accurately guess chrome
+#' versions without too much pain.
+#'
+#' @export
+get_chrome_list <- function() {
+  
+  ### Checks to see if chrome is installed
+  chromeplat <- wdman:::chrome_check(verbose = T)[['platform']]
+  
+  ### Actually gets the chrome versions
+  raw_chrome_ver <- binman::list_versions('chromedriver')[[chromeplat]]
+  
+  ### Filters out some junk versions that don't actually exist
+  raw_chrome_ver <- raw_chrome_ver[grepl('^.*\\..*\\..*\\..*$', raw_chrome_ver)]
+  
+  ### Returns the list of chrome versions
+  raw_chrome_ver
+  
+}
